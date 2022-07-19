3967253ba902133d965ac97f2912834643bc5699
Theo <poette.theo@gmail.com>
2022-07-19 09:34:40 +0200

    feat: testing fouc

---
+++ components/Header/Header.styles.ts
@@ -0,0 +1,34 @@
+import { createStyles } from '@mantine/core';
+
+export default createStyles((theme) => ({
+  links: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  link: {
+    display: "block",
+    lineHeight: 1,
+    padding: "8px 12px",
+    borderRadius: theme.radius.sm,
+    textDecoration: "none",
+    color: theme.white,
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    "&:hover": {
+      backgroundColor: theme.colors.dark[3],
+    },
+  },
+
+  linkLabel: {
+    marginRight: 5,
+  },
+}));
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,39 +9,7 @@ import {
   Title,
 } from "@mantine/core";
 import Sidebar from "../Sidebar";
-
-const useStyles = createStyles((theme) => ({
-  links: {
-    [theme.fn.smallerThan("sm")]: {
-      display: "none",
-    },
-  },
-
-  burger: {
-    [theme.fn.largerThan("sm")]: {
-      display: "none",
-    },
-  },
-
-  link: {
-    display: "block",
-    lineHeight: 1,
-    padding: "8px 12px",
-    borderRadius: theme.radius.sm,
-    textDecoration: "none",
-    color: theme.white,
-    fontSize: theme.fontSizes.sm,
-    fontWeight: 500,
-
-    "&:hover": {
-      backgroundColor: theme.colors.dark[3],
-    },
-  },
-
-  linkLabel: {
-    marginRight: 5,
-  },
-}));
+import useStyles from "./Header.styles";
 
 interface Links {
   links: {
